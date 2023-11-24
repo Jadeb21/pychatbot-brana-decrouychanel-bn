a7cf1004e7f42a007a779616ab13172ba9cf0d92
Jadeb21 <151123659+Jadeb21@users.noreply.github.com>
2023-11-24 09:30:36 +0100

    Add files via upload
    Signed-off-by: Jadeb21 <151123659+Jadeb21@users.noreply.github.com>

--- Fonction.py
+++ Fonction.py
@@ -11,8 +11,31 @@ def print_list(l):
 def extraire_nom(l):
     list_nom=[]
     for i in l:
-        if i[-5]<=chr(90):
+        if i[-5]<chr(90):
             list_nom.append(i[11:-5])
-        else :
+        else:
             list_nom.append(i[11:-4])
+    for nom in list_nom :
+        if list_nom.count(nom)>1:
+            list_nom.remove(nom)
+    if nom == "Chirac":
+        print("Jacques")
+    if nom == "Giscard dEstaing":
+        print("Valéry")
+    if nom == "Hollande":
+        print("François")
+    if nom == "Macron":
+        print("Emmanuelle")
+    if nom == "Mitterrand":
+        print("François")
+    if nom == "Sarcozy":
+        print("Nicolas")
     return list_nom
+
+def minucule(l):
+    for line in list_of_files :
+        for letter in line:
+          letter = ord(letter)
+            if letter > chr(97):
+                letter = ord(letter) - 32
+                print(letter)
